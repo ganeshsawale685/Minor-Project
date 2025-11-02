33c8b39b85fbc25bc45159bcc95923a8fd5fe821
Sigma Student <ganeshsawale685@gmail.com>
2025-11-02 12:01:42 +0530

    Add Home page components: Hero, AiTools, and Testimonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/testimonial'
+
 
 const Home = () => {
   return (
     <>
      <Navbar/>
-      
+     <Hero/>
+     <AiTools/>
+     <Testimonial/>
     </>
   )
 }
